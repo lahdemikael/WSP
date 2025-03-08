fbd3d15bba266edfdb54f7d48cd0c2ff1750ce2b
lahdemikael <147116855+lahdemikael@users.noreply.github.com>
2025-03-08 14:04:48 +0200

    Delete E01 directory

--- E01/Task4/systemUtils.mjs
+++
@@ -1,8 +0,0 @@
-function formatSystemInfo(uptime, totalMemory) {
-    return JSON.stringify({
-      uptime: uptime + ' seconds',
-      totalMemory: totalMemory + ' MB'
-    });
-  }
-  
-  export { formatSystemInfo };
