b5795f69817fa861aa2ba70ff68c1da2c14702b4
lahdemikael <147116855+lahdemikael@users.noreply.github.com>
2025-03-08 14:08:14 +0200

    Delete E01 directory

--- E01/Task1/task1.js
+++
@@ -1,4 +0,0 @@
-console.log("Username:", process.env.USERNAME);
-console.log("Current Working Directory:", process.cwd());
-console.log("Node.js Version:", process.version);
-console.log("Platform:", process.platform);
--- E01/Task3/systemUtils.js
+++
@@ -1,8 +0,0 @@
-function formatSystemInfo(uptime, totalMemory) {
-  return JSON.stringify({
-    uptime: uptime + ' seconds',
-    totalMemory: totalMemory + ' MB'
-  });
-}
-
-module.exports = { formatSystemInfo };
